1da45255646d3bb977fdd272f1a02d996ced40ad
Ainsof So'o <ainsofs@sprep.org>
2022-05-27 18:20:54 +1300

    login and registration fields added

--- q-app/quasar-project/src/pages/AuthPage.vue
+++ q-app/quasar-project/src/pages/AuthPage.vue
@@ -1,19 +1,55 @@
 <template>
   <q-page class="q-pa-md col">
-    <q-list bordered padding>
-      <q-item-label header>Auth</q-item-label>
+    <div class="q-gutter-y-md" style="max-width: 600px">
+      <q-card>
+        <q-tabs
+          v-model="tab"
+          dense
+          class="text-grey"
+          active-color="primary"
+          indicator-color="accent"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="login" label="Login" />
+          <q-tab name="register" label="Register" />
+        </q-tabs>
 
-      <q-item tag="label" v-ripple>
-        <q-item-section>
-          <q-item-label>Dev Mode</q-item-label>
-          <q-item-label caption>Submit tests to a Demo form.</q-item-label>
-        </q-item-section>
-        <q-item-section side top>
-          <q-toggle v-model="isDevMode" val="friend" />
-        </q-item-section>
-      </q-item>
+        <q-separator />
 
-    </q-list>
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="login">
+
+            <div class="q-gutter-md q-pb-md" >
+              <q-input outlined label="Email address" hint="I-meli" class="col q-pr-md" :rules="[ val => val.length || 'Please enter your email.' ]" />
+
+              <q-input outlined label="Password" type="password" hint="Faalilolilo" class="col q-pr-md" :rules="[ val => val.length || 'Please enter your password.' ]" />
+
+              <!-- TODO add forgot password -->
+              <div class="text-right">
+                <q-btn type="submit" flat color="primary" label="Login" class="btn-submit" />
+              </div>
+            </div>
+
+          </q-tab-panel>
+
+          <q-tab-panel name="register">
+            <div class="q-gutter-md q-pb-md" >
+
+              <p>Register to access your test results from any device!
+              </p>
+              <q-input outlined label="Email address" hint="I-meli" class="col q-pr-md" :rules="[ val => val.length || 'Please enter your email.' ]" />
+
+              <q-input outlined label="Password" type="password" hint="Faalilolilo" class="col q-pr-md" :rules="[ val => val.length || 'Please enter your password.' ]" />
+
+              <div class="text-right">
+                <q-btn type="submit" flat color="primary" label="Register" class="btn-submit" />
+              </div>
+            </div>
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
@@ -32,10 +68,7 @@ export default defineComponent({
   },
   data() {
     return {
-      isDarkMode: this.store.settings.isDarkMode,
-      warnDuplicate: this.store.settings.warnDuplicate,
-      showTooltips: this.store.settings.showTooltips,
-      isDevMode: this.store.settings.isDevMode,
+      tab: "login"
     }
   }
 })
