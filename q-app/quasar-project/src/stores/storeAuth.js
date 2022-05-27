7c084cf62b6d5c751d7e674fc3bc811e17bc8965
Ainsof So'o <ainsofs@sprep.org>
2022-05-27 20:34:04 +1300

    user logged in

--- q-app/quasar-project/src/stores/storeAuth.js
+++ q-app/quasar-project/src/stores/storeAuth.js
@@ -1,6 +1,9 @@
 import { defineStore } from 'pinia'
 import { firebaseAuth } from 'boot/firebase'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth"
 import { Notify } from "quasar"
 
 export const useStoreAuth = defineStore("storeAuth", {
@@ -29,15 +32,48 @@ export const useStoreAuth = defineStore("storeAuth", {
             icon: "announcement" })
         })
         .catch((error) => {
+          var errorCode = error.code
+          var errorMessage = error.message
+          if (errorCode === "auth/email-already-in-use") {
+            errorMessage = "Email already in use."
+          }
+
           Notify.create({
-            message: error.message,
+            message: errorMessage,
             icon: "warning",
             color: "warning",
           })
+          console.log(error)
         })
     },
     loginUser(userDetails) {
-      console.log('loginUser: ', userDetails)
+      signInWithEmailAndPassword(
+        firebaseAuth,
+        userDetails.email,
+        userDetails.password
+      )
+        .then((response) => {
+          console.log(response)
+
+          Notify.create({
+            message: "Logged in",
+            icon: "announcement",
+          })
+        })
+        .catch((error) => {
+          var errorCode = error.code
+          var errorMessage = error.message
+          if (errorCode === "auth/wrong-password") {
+            errorMessage = "Wrong password."
+          }
+
+          Notify.create({
+            message: errorMessage,
+            icon: "warning",
+            color: "warning",
+          })
+          console.log(error)
+        })
     }
   },
 })
