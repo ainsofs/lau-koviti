cc787eeee77307d1dcd21732f24a7b81cf0b657c
Ainsof So'o <ainsofs@sprep.org>
2024-04-21 02:13:02 +0000

    use ipv6

--- q-app/quasar-project/quasar.config.js
+++ q-app/quasar-project/quasar.config.js
@@ -85,7 +85,6 @@ module.exports = configure(function (ctx) {
       },
       port: 8080,
       open: false, // opens browser window automatically
-      host: '0.0.0.0', // listen on all IPs
 
       proxy: {
         // proxy requests to fix cors error. also update public/_redirects file
